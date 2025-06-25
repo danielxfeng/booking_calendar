9c99f72378dccc18573a8e41b167166f9d4281b9
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-25 18:53:58 +0300

    feat: enhance error handling with additional error types for API responses

--- src/lib/errorHandler.ts
+++ src/lib/errorHandler.ts
@@ -6,6 +6,8 @@
  * @contact intra: @xifeng
  */
 
+import type { AxiosError } from 'axios';
+
 /**
  * @summary An error received from the API.
  * @param message the message to throw
@@ -14,4 +16,22 @@ const ThrowInvalidIncomingDataErr = (message: string) => {
   throw new Error(`Status: 502. Message: The data from API is illegal: ${message}`);
 };
 
-export { ThrowInvalidIncomingDataErr };
+/**
+ * @summary An unexpected error.
+ * @param message the message to throw
+ */
+const ThrowInternalError = (message: string) => {
+  throw new Error(`Status: 500. Message: An unexpected error: ${message}`);
+};
+
+/**
+ * @summary Throws an error from axios.
+ * @param error The AxiosError object.
+ */
+const ThrowAxiosError = (error: AxiosError) => {
+  throw new Error(
+    `Status: ${error.response?.status ?? 500}. Message: An error from API: ${JSON.stringify(error.response?.data)}`,
+  );
+};
+
+export { ThrowAxiosError, ThrowInternalError, ThrowInvalidIncomingDataErr };
