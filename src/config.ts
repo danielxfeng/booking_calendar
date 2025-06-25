d2bfc06a69335b0fec6b45ce5f44b87b4e4e7c9d
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-25 18:50:04 +0300

    feat: add API configuration constants for authentication and fetching

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,36 @@
  */
 const NUMBERS_OF_ROOMS: number = 2;
 
+/**
+ * The time slot interval
+ */
 const TIME_SLOT_INTERVAL = 15; // Must divide evenly into 60 (e.g., 5, 10, 15, 30).
 
-export { NUMBERS_OF_ROOMS, TIME_SLOT_INTERVAL };
+/**
+ * The API Url
+ */
+const API_URL = '';
+
+/**
+ * The API endpoint for Login
+ */
+const ENDPOINT_AUTH = '';
+
+/**
+ * The API endpoint for getting slots
+ */
+const ENDPOINT_SLOTS = '';
+
+/**
+ * The timeout for API fetching
+ */
+const FETCHER_TIMEOUT = 30; // 30 seconds
+
+export {
+  API_URL,
+  ENDPOINT_AUTH,
+  ENDPOINT_SLOTS,
+  FETCHER_TIMEOUT,
+  NUMBERS_OF_ROOMS,
+  TIME_SLOT_INTERVAL,
+};
