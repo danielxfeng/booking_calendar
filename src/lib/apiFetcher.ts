2147d6a9c011512e126f3e8829120acdeaaeeec8
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-25 19:18:07 +0300

    refactor: update getSlots function to return SlotsRooms directly instead of AxiosResponse

--- src/lib/apiFetcher.ts
+++ src/lib/apiFetcher.ts
@@ -6,7 +6,6 @@
  * @contact intra: @xifeng
  */
 
-import type { AxiosResponse } from 'axios';
 import { formatISO, nextSunday } from 'date-fns';
 
 import { ENDPOINT_SLOTS } from '@/config';
@@ -18,14 +17,15 @@ import type { SlotsRooms } from '@/lib/schema';
  * @param start start ISO date string (e.g. "2025-06-23"), should be Monday
  * @returns
  */
-const getSlots = async (start: string): Promise<AxiosResponse<SlotsRooms>> => {
+const getSlots = async (start: string): Promise<SlotsRooms> => {
   const end = formatISO(nextSunday(new Date(start)), { representation: 'date' });
 
   const params = new URLSearchParams();
   params.set('start', start);
   params.set('end', end);
 
-  return axiosFetcher.get(ENDPOINT_SLOTS, { params });
+  const res = await axiosFetcher.get<SlotsRooms>(ENDPOINT_SLOTS, { params });
+  return res.data;
 };
 
 export { getSlots };
