45ec6ef6653bb25f3ae269bc5361b4d11a33f117
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-27 21:22:55 +0300

    refactor: update type references for getSlots function to use Rooms

--- src/lib/apiFetcher.ts
+++ src/lib/apiFetcher.ts
@@ -10,21 +10,21 @@ import { formatISO, nextSunday } from 'date-fns';
 
 import { ENDPOINT_SLOTS } from '@/config';
 import { axiosFetcher } from '@/lib/axiosFetcher';
-import type { SlotsRooms } from '@/lib/schema';
+import type { Rooms } from '@/lib/schema';
 
 /**
  * @summary Fetch available slots for a given week.
  * @param start start ISO date string (e.g. "2025-06-23"), should be Monday
  * @returns
  */
-const getSlots = async (start: string): Promise<SlotsRooms> => {
+const getSlots = async (start: string): Promise<Rooms> => {
   const end = formatISO(nextSunday(new Date(start)), { representation: 'date' });
 
   const params = new URLSearchParams();
   params.set('start', start);
   params.set('end', end);
 
-  const res = await axiosFetcher.get<SlotsRooms>(ENDPOINT_SLOTS, { params });
+  const res = await axiosFetcher.get<Rooms>(ENDPOINT_SLOTS, { params });
   return res.data;
 };
 
