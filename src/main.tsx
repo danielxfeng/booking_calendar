3ee531997b78e5007cd373818a3c8c06ce1b7d07
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-07-04 09:59:51 +0300

    fix: remove unused Sentry import

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import * as Sentry from '@sentry/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from '@/App';
