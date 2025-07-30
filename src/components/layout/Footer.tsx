0a1236baf87cab82a26399c7c82dd28971a9460a
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-07-30 19:11:21 +0300

    fix: shorter the footer on mobile view.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,28 +2,29 @@ import { Heart } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className='bg-accent flex h-14 items-center justify-center border-t'>
-      <div className='text-muted-foreground text-center text-sm flex items-center gap-2'>
+    <footer className='bg-accent flex h-8 lg:h-14 items-center justify-center border-t'>
+      <div className='text-muted-foreground flex items-center gap-2 text-center text-xs lg:text-sm'>
         <span>Developed with</span>
-        <Heart className='h-4 w-4 fill-sky-400 stroke-0 animate-pulse' /> 
-        <span>by{' '}
-        <a
-          href='https://www.linkedin.com/in/xin-daniel-feng'
-          target='_blank'
-          rel='noreferrer'
-          className='hover:underline'
-        >
-          xifeng
-        </a>{' '}
-        and{' '}
-        <a
-          href='https://github.com/ibnBaqqi'
-          target='_blank'
-          rel='noreferrer'
-          className='hover:underline'
-        >
-          sabdulba
-        </a>
+        <Heart className='h-4 w-4 animate-pulse fill-sky-400 stroke-0' />
+        <span>
+          by{' '}
+          <a
+            href='https://www.linkedin.com/in/xin-daniel-feng'
+            target='_blank'
+            rel='noreferrer'
+            className='hover:underline'
+          >
+            xifeng
+          </a>{' '}
+          and{' '}
+          <a
+            href='https://github.com/ibnBaqqi'
+            target='_blank'
+            rel='noreferrer'
+            className='hover:underline'
+          >
+            sabdulba
+          </a>
         </span>
       </div>
     </footer>
